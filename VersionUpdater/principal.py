cc2c5219329fef1e9bd54de6927a4384e26c8643
apaza610 <homar.orozco@gmail.com>
2024-05-28 23:35:11 -0400

    Update messages

--- VersionUpdater/principal.py
+++ VersionUpdater/principal.py
@@ -16,14 +16,14 @@ def unzip_and_move(src_zip, dest_dir):
 	temporal = src_zip.split('\\')[-1].replace('.zip','')
 
 	os.rename(f"{dest_dir}\\{temporal}", f"{dest_dir}\\blender")
-	print(f"Unzipped files fueron movidos a {dest_dir}")
+	print(f"Unzipped files were moved to... {dest_dir}")
 
 os.chdir(r"E:\win\Downloads")
 files = [f for f in os.listdir() if (f.startswith('blender') and f.endswith('.zip'))]
 files.sort(key=os.path.getmtime)
 
 # src_zip = input("path of new Blender: ")	#'E:\\win\\Downloads\\blender-4.1.0-beta+v41.a677518cb50a-windows.amd64-release.zip'
-src_zip = files[-1]		#descomprimiendo el ultimo (mas nuevo) de la lista
+src_zip = files[-1]		#descomprimiendo el ultimo de los zip (mas nuevo) de la lista
 
 if os.path.exists(src_zip):
 	print(f"desComprimiendo: {src_zip}")
