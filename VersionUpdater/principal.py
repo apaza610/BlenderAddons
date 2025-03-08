e56316ab895a385cd32923e6dad1abb19b5c4332
apaza610 <homar.orozco@gmail.com>
2025-03-08 08:42:56 -0400

    borrar zips mas viejos

--- VersionUpdater/principal.py
+++ VersionUpdater/principal.py
@@ -1,31 +1,40 @@
-#Este script es para actualizar blender portable a la ultima descargada (segun fecha)
 import zipfile
 import shutil
 import os
 import sys
 
-def unzip_and_move(src_zip, dest_dir):
-	if not os.path.exists(dest_dir):
-		os.makedirs(dest_dir)
+folderDwnld = "E:\\win\\Downloads"
+folderDstno = "E:\\apps\\art3d"
+folderFinal = f"{folderDstno}\\blender-4.4"
 
-	shutil.rmtree(f"{dest_dir}\\blender")
+os.chdir(folderDwnld)
+
+def unzip_and_move(src_zip):
+	if os.path.exists(folderFinal):
+		shutil.rmtree(folderFinal)		# borrar lo viejo
 
 	with zipfile.ZipFile(src_zip, 'r') as zip_ref:
-		zip_ref.extractall(dest_dir)
+		zip_ref.extractall(folderDstno)
 
-	temporal = src_zip.split('\\')[-1].replace('.zip','')
+	# temporal = src_zip.split('\\')[-1].replace('.zip','')
+	temporal = f"{folderDstno}\\{src_zip}"		# E:\\apps\\art3d\\blender-4.4.0-beta+v44.b39c637d63ec-windows.amd64-release.zip
+	temporal = temporal.replace('.zip','')
 
-	os.rename(f"{dest_dir}\\{temporal}", f"{dest_dir}\\blender")
-	print(f"Unzipped files were moved to... {dest_dir}")
+	os.rename(temporal, folderFinal)
+	print(f"Unzipped files fueron movidos a {folderDstno}")
 
-os.chdir(r"E:\win\Downloads")
+##########################################################################################################
 files = [f for f in os.listdir() if (f.startswith('blender') and f.endswith('.zip'))]
-files.sort(key=os.path.getmtime)
+files.sort(key=os.path.getmtime, reverse=False)
 
 # src_zip = input("path of new Blender: ")	#'E:\\win\\Downloads\\blender-4.1.0-beta+v41.a677518cb50a-windows.amd64-release.zip'
-src_zip = files[-1]		#descomprimiendo el ultimo de los zip (mas nuevo) de la lista
+src_zip = files[-1]		#descomprimiendo el ultimo (mas nuevo) de la lista
 
 if os.path.exists(src_zip):
 	print(f"desComprimiendo: {src_zip}")
 
-unzip_and_move(src_zip ,'E:\\apps\\art3d')
+unzip_and_move(src_zip)
+
+for zip in files:
+	if zip != src_zip:
+		os.remove(zip)		#borrar los otros zips
